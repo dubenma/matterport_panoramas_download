5ed284c8c25dfdc71a8ab10d0da16bc3fc581afd
dubenma <89923311+dubenma@users.noreply.github.com>
2021-09-21 11:44:10 +0200

    csv with poses now includes the names of panoramas

--- download_poses.py
+++ download_poses.py
@@ -3,9 +3,18 @@ import json
 import os.path
 import requests
 from requests.auth import HTTPBasicAuth
+
+path = r'C:\Users\zsd\CIIRC\data\matterport\Broca Hospital with Curtains'
+space_id = 'N89M9ANY82h'
+
+# path = r'C:\Users\zsd\CIIRC\data\matterport\Broca Hospital without Curtains'
+# space_id = '6ak4XqnRK9c'
+
+# path = r'C:\Users\zsd\CIIRC\data\matterport\Broca Living Lab without Curtains'
+# space_id = 'QjeCDLSainK'
   
-path = r'C:\Users\zsd\CIIRC\data\matterport\Broca Hospital without Curtains'
-space_id = '6ak4XqnRK9c'
+# path = r'C:\Users\zsd\CIIRC\data\matterport\Broca Living Lab with Curtains'
+# space_id = '8RCXJAxiDAA'
 
 matterport_id = ''
 secret_token = ''
@@ -19,9 +28,13 @@ l = len(data['data']['model']['locations'])
 
 with open(os.path.join(path,'poses.csv'), 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',')
+    header = ['id', 'panoramas', 'pano_pos_x','pano_pos_y','pano_pos_z','pano_ori_w','pano_ori_x','pano_ori_y','pano_ori_z']
+    spamwriter.writerow(header)
 
     for i in range(l):
         pano = data['data']['model']['locations'][i]['panos'][0]
+        
+        name = 'pano'+str(i)+'.jpg'
         
         pos_x = pano['position']['x']
         pos_y = pano['position']['y']
@@ -32,7 +45,7 @@ with open(os.path.join(path,'poses.csv'), 'w', newline='') as csvfile:
         rot_z = pano['rotation']['z']
         rot_w = pano['rotation']['w']
         
-        row = [pos_x, pos_y, pos_z, rot_x, rot_y, rot_z, rot_w]
+        row = [i,name,pos_x, pos_y, pos_z, rot_w, rot_x, rot_y, rot_z]
         
         spamwriter.writerow(row)
 
